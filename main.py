07d5d19cee636d23122aec468821408fd8e45e91
AurelienAudero <64486562+AurelienAudero@users.noreply.github.com>
2023-12-17 16:57:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from tkinter import Tk, Label, Radiobutton, Button, IntVar, messagebox
 #   PROGRAMME DU JEU   #
 ########################
 class Personnage:
-  def __init__(self, x, y, keybinds):
+  def __init__(self, x, y, width, height, keybinds):
     self.x = x
     self.y = y
+    self.width = width
+    self.height = height
     self.keybinds = keybinds
 
     # Détermination des touches pour contrôler le personnage
@@ -31,25 +33,47 @@ class Personnage:
       if (self.x > 0) :
         self.x = self.x - 10
     if pyxel.btn(self.personnageBas):
-      if (self.y < 720) :
+      if (self.y < resHauteur-self.height) :
         self.y = self.y + 10
     if pyxel.btn(self.personnageDroite):
-      if (self.x < 720) :
+      if (self.x < resLongueur-self.width) :
         self.x = self.x + 10
           
   def draw(self):
-    pyxel.rect(self.x, self.y, 50, 80, 9)
+    pyxel.rect(self.x, self.y, self.width, self.height, 9)
+
+class Zombie:
+  def __init__(self, x, y, width, height):
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+
+  """
+  def move(self):
+    if pyxel.btn(self.personnageHaut):
+      if (self.y > 0) :
+        self.y = self.y - 10
+    if pyxel.btn(self.personnageGauche):
+      if (self.x > 0) :
+        self.x = self.x - 10
+    if pyxel.btn(self.personnageBas):
+      if (self.y < resHauteur-self.height) :
+        self.y = self.y + 10
+    if pyxel.btn(self.personnageDroite):
+      if (self.x < resLongueur-self.width) :
+        self.x = self.x + 10
+  """
+          
+  def draw(self):
+    pyxel.rect(self.x, self.y, self.width, self.height, 9)
 
 class Jeu:
   def __init__(self, l, h, keybinds):
     pyxel.init(l, h, title = "Kino der toten")
 
-    # Sauvegarde de la résolution de la fenêtre
-    self.resLongueur = l
-    self.resHauteur = h
-
     # Création du personnage
-    self.personnage = Personnage(360, 240, keybinds)
+    self.personnage = Personnage(360, 240, 50, 80, keybinds)
 
     pyxel.run(self.update, self.draw)
 
@@ -109,5 +133,8 @@ while userChosenKeybinds == 0 :
     messagebox.showwarning("Erreur", "Il est nécéssaire de choisir une méthode d'entrée")
 
 # Lancement du jeu
+
 if (userChosenKeybinds == 1) or (userChosenKeybinds == 2):
-  Jeu(720, 480, userChosenKeybinds)
+  resLongueur = 720
+  resHauteur = 480
+  game = Jeu(resLongueur, resHauteur, userChosenKeybinds)
